0cf54e807ca22b1af416adeef7691f387618a08d
Ashkore <saboyer@ollusa.edu>
2016-10-19 23:44:06 -0500

    Revert "Merge pull request #5 from Ashkore/my-testing-branch"
    This reverts commit 577aac29f1c6424d05599215625ad4dcf3e9be54, reversing
    changes made to 9ab25ce8e261953cdbe9a266eadd7bd458ab2ebd.

--- IOS-Swift-Sean-Vivek/NewIdeaViewController.swift
+++ IOS-Swift-Sean-Vivek/NewIdeaViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class NewIdeaViewController: UIViewController {
 
-    @IBOutlet var newIdeaScrollView: UIScrollView!
-    
     @IBOutlet var _newIdeaName: UITextField!
     
     override func viewDidLoad()
