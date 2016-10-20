3822aa15c760772a0e682cceda23846782295632
Ashkore <saboyer@ollusa.edu>
2016-10-19 23:32:54 -0500

    Committing Static Data and Idea class

---
+++ IOS-Swift-Sean-Vivek/SampleData.swift
@@ -0,0 +1,15 @@
+//
+//  SampleData.swift
+//  IOS-Swift-Sean-Vivek
+//
+//  Created by Sean Boyer on 10/19/16.
+//
+//
+
+import Foundation
+let IdeasData = [
+    Idea(name: "One", date: Date(), symbol: "1", gps: 1.11),
+    Idea(name: "Two", date: Date(), symbol: "1", gps: 1.11),
+    Idea(name: "Three", date: Date(), symbol: "1", gps: 1.11),
+    Idea(name: "Four", date: Date(), symbol: "1", gps: 1.11)
+];
